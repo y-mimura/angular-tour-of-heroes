2067a1e270ccbd8a008e4a6d23ccfa667ecc2982
y-mimura <y.mimura1995@gmail.com>
2019-06-08 20:46:33 +0900

    tour of heroes 完了

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+
+  hero: Hero;
+
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private heroService: HeroService
+  ) { }
+
+  ngOnInit() {
+    this.getHero();
+  }
+
+  getHero() {
+    // route.snapshotはコンポーネントが作成された直後のルート情報の静的イメージ
+    // paramMapは、URL から抽出されたルートパラメータ値の辞書
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack() {
+    this.location.back();
+  }
+
+  save() {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
+}
